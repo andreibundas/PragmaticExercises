73a924693351ec4bf7736b77b6606de45141d61e
Andrei Bundas <andrei.bundas@gmail.com>
2020-06-30 21:32:02 +0300

    Commit "CountingNoCharacters"

---
+++ src/main/java/org/inputprocessingoutput/CountingNrCharacters.java
@@ -0,0 +1,19 @@
+package org.inputprocessingoutput;
+
+import java.util.Scanner;
+
+public class CountingNrCharacters {
+
+
+    public static void main( String[] args ) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("What is the input string ? ");
+
+        String input1 = input.next();
+        System.out.println(input1 + " has " + input1.length() + " characters");
+    }
+
+
+}
